07141e3a7f0ad6367f98f39e3d4390f8778f64fc
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-05-01 02:46:56 -0300

    feat(front): better box and card component

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html style={{ overflow: "hidden" }}>
       <body>
         {children}
       </body>
